0206736d5ebcf52d083ec267e700aa16ff97e86f
YaneriPerdomo <92409304+YaneriPerdomo@users.noreply.github.com>
2021-11-20 21:13:49 -0400

    Add files via upload

---
+++ Do-While2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    char opc;
+
+
+
+    do{
+            printf(Hola profesor);
+            printf("/ndigite "s" para saludar nuevamente");
+            scanf("%c");
+            1++;
+    }while(opc == "S");
+
+
+    return 0;
+
+    }
